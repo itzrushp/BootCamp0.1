d8333daec65a19d2ae79d15f5a7ac6e6f2aca5be
Rush <itzashert@gmail.com>
2026-02-03 20:59:58 +0530

    in progress

--- Day02 - Async JS/callbacks/easy/rejectAfter.js
+++ Day02 - Async JS/callbacks/easy/rejectAfter.js
@@ -3,6 +3,12 @@
 // You are required to create a function named rejectAfter that accepts a time duration in milliseconds. 
 // The function should return a Promise that waits for the specified time and then rejects.
 
-function rejectAfter(ms) {}
+function rejectAfter(ms) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            reject();
+        },ms)
+    })
+}
 
 module.exports = rejectAfter;
