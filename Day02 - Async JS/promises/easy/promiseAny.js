19724f9a0cd58afbb88083874edc869b77d723f8
Rush <itzashert@gmail.com>
2026-02-19 19:22:54 +0530

    PASSED

--- Day02 - Async JS/promises/easy/promiseAny.js
+++ Day02 - Async JS/promises/easy/promiseAny.js
@@ -3,6 +3,33 @@
 // You are required to implement a function named promiseAny that accepts an array of Promises. 
 // The function should return a new Promise that resolves immediately when any one of the input promises resolves successfully. 
 // If all the promises reject, the returned Promise should reject with an error.
-function promiseAny(promises) {}
+function promiseAny(promises) {
+    return new Promise((resolve , reject)=>{
+        if(promises.length===0) {
+            reject(new Error("Empty iterable"));
+            return;
+        }
+
+        let rejectCount = 0;
+
+        promises.forEach((promise, index)=>{
+
+            Promise.resolve(promise) // making sure that if its a value then returned automatically
+                .then((result)=>{
+                    resolve(result); // immediatly resolves 
+                    return; 
+                })
+                .catch(()=>{
+                    rejectCount++;
+                    if(rejectCount === promises.length){
+                        reject(new Error("All promises were rejected"));
+                        return;
+                    }
+                })
+        })
+    })
+}
 
 module.exports = promiseAny;
+
+// PASSED 
