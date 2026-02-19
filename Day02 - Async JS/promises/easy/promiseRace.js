e9112e05651be02533e256e6093a5d86fcccfb7a
Rush <itzashert@gmail.com>
2026-02-19 19:28:18 +0530

    PASSED

--- Day02 - Async JS/promises/easy/promiseRace.js
+++ Day02 - Async JS/promises/easy/promiseRace.js
@@ -4,6 +4,26 @@
 // The function should accept an iterable of values that may include Promises or plain values. 
 // It must settle as soon as the first input settles, resolving or rejecting accordingly. 
 // Using Promise.resolve ensures non-promise values are handled correctly.
-function promiseRace(promises) {}
+function promiseRace(promises) {
+    return new Promise((resolve, reject) => {
+        if(promises.length === 0) {  //stays pending for empty Promises 
+            return;
+        }
+        
+        promises.forEach((promise, index)=>{
+            Promise.resolve(promise)
+                .then((returnedAnswer)=>{
+                    resolve(returnedAnswer);
+                    return;
+                })
+                .catch((err)=>{
+                    reject(err);
+                    return;
+                })
+        })
+    })
+}
 
 module.exports = promiseRace;
+
+// VARDICT => 
